77c05754919c623be5ebb44883cb463a4bec1599
Michał Okulewicz <m.okulewicz.phd@gmail.com>
2022-01-12 18:11:26 +0100

    Initial tests

---
+++ SampleLibSE1.Test/RouteTest.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleLibSE1.Test
+{
+    [TestClass]
+    public class RouteTest
+    {
+        private const double EPS = 1e-8;
+
+        [TestMethod]
+        public void AddPointsToRouteTest()
+        {
+            Location location1 = new Location();
+            Location location2 = new Location();
+            Route route = new Route();
+            const double ExpectedInitialRouteLength = 0.0;
+            const double ExpectedRouteLengthAfterOnePoint = 0.0;
+            const double ExpectedRouteLengthAfterTwoPoints = 1.0;
+
+            Assert.AreEqual(ExpectedInitialRouteLength, route.Length, EPS);
+            route.AddLocation(location1);
+            Assert.AreEqual(ExpectedRouteLengthAfterOnePoint, route.Length, EPS);
+            route.AddLocation(location2);
+            Assert.AreEqual(ExpectedRouteLengthAfterTwoPoints, route.Length, EPS);
+
+        }
+    }
+}
---
+++ SampleLibSE1/Route.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SampleLibSE1
+{
+    public class Route
+    {
+        public double Length { get; private set; }
+
+        public void AddLocation(Location location1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
