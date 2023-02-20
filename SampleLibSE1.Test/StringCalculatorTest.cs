9a7608e7a6ce5b95868e799a9e7e313333f7ce26
Michał Okulewicz <m.okulewicz.phd@gmail.com>
2023-02-20 18:52:03 +0100

    non passing test for sum

--- SampleLibSE1.Test/StringCalculatorTest.cs
+++ SampleLibSE1.Test/StringCalculatorTest.cs
@@ -15,5 +15,15 @@ namespace SampleLibSE1.Test
             int result = sc.Add(emptyInput);
             Assert.AreEqual(expectedOutputForEmptyInput, result);
         }
+
+        [TestMethod]
+        public void TwoNumberCommaDelimetedReturnSum()
+        {
+            string nonEmptyInput = "1,2";
+            int expectedOutputForNonEmptyInput = 3;
+            StringCalculator sc = new StringCalculator();
+            int result = sc.Add(nonEmptyInput);
+            Assert.AreEqual(expectedOutputForNonEmptyInput, result);
+        }
     }
 }
